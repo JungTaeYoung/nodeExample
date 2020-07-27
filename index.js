72a8147b0f5ce871d5036eedd2a4a3c5f8c4d604
jung <young1114@mirimmeidalab.co.kr>
2020-07-27 20:33:08 +0900

    password config

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const express = require('express')
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://young:server1004!@management.bma47.mongodb.net/<dbname>?retryWrites=true&w=majority', {
-    useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify: false
-}).then(()=> console.log('MongoDB Connected...'))
-  .catch(err => console.log(err))
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+
+const config = require('./config/key')
+
+const { User } = require('./models/User')
 
 
-const app = express()
-const port = 3000
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+mongoose.connect(config.mongoURI, {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
+
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/register', (req, res) => {
+    // 회원가입
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
