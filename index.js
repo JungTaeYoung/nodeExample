9b23188285c6d0b75851f863fe8aa644b34d2602
jung <young1114@mirimmeidalab.co.kr>
2020-07-28 00:15:32 +0900

    jwt and token

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const config = require('./config/key')
 
@@ -11,6 +12,8 @@ const { User } = require('./models/User')
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
+
+app.use(cookieParser())
 
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
@@ -28,6 +31,31 @@ app.post('/register', (req, res) => {
         if(err) return res.json({ success: false, err })
         return res.status(200).json({
             success: true
+        })
+    })
+})
+
+app.post('/login', (req, res) => {
+    User.findOne({ email: req.body.email }, (err, user)=> {
+        if(!user) {
+            return res.json({
+                loginSuccess: false,
+                massage: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+
+        user.comparePassword(req.body.password, (err, isMatch)=> {
+            if(!isMatch) {
+                return res.json({ loginSuccess: false, massage: "비밀2번호가 틀렸습니다." });
+            }
+
+            user.generateToken((err, user)=>{
+                if(err) return res.status(400).send(err);
+                
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({ loginSuccess: true, userId: user._id })
+            })
         })
     })
 })
